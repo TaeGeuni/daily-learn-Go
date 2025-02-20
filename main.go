b2f9a8718992f5816e919b597e79a216193b43df
Taegeuni <qoxorms1504@gmail.com>
2025-02-20 13:13:12 +0900

    Learn-Testing_Benchmark✅

--- main.go
+++ main.go
@@ -1,27 +1,5 @@
 package main
 
-import "fmt"
-
-type Report interface {
-	Report() string
-}
-
-type MarketingReport struct {
-	report string
-}
-
-func (r *MarketingReport) Report() string {
-	return r.report
-}
-
-func ReportSend(report Report, email string) {
-	fmt.Printf("%s 여기로 보냈습니다\n", email)
-}
-
 func main() {
-	email := "btg0805@example.com"
-	var data MarketingReport
-	data.report = "안녕하세요"
 
-	ReportSend(&data, email)
 }
